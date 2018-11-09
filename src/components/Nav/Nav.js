d3680db8543924846543d1ecae61de6b8e14dd3c
Silu Hu <silu@Silus-MacBook-Pro.local>
2018-11-09 13:45:29 -0500

    add homework

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavMessage from "../NavMessage";
+import "./Nav.css";
+
+// Component for the Navbar
+
+const Nav = props => (
+  <nav className="navbar">
+    <ul>
+      <li className="brand">
+        <p>Stuff Curry Clicky-Game</p>
+      </li>
+      <NavMessage score={props.score} topScore={props.topScore} />
+      <li>
+        Score: {props.score} | Top Score: {props.topScore}
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
